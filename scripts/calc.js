47c366793b0a749e148b963c7ebe7f0e38e94ff3
AlexNexton <cianoc13@gmail.com>
2020-06-10 15:35:59 +0000

    doc: added error string message

---
+++ scripts/calc.js
@@ -0,0 +1,9 @@
+//Addition Function
+function addition(numOne,numTwo){
+if(typeof(numOne) == "number"&& typeof(numTwo)== "number"){
+    return numOne+numTwo;
+}
+else{
+    return "Error!";
+}
+};
---
+++ spec/calcSpec.js
@@ -0,0 +1,17 @@
+describe ("calculator", function(){
+
+    describe("Addition Test" ,function(){
+
+        it("should return 42", function(){
+
+            expect(addition(20,22)).toBe(42);
+        });
+        it ("should return 4", function(){
+              expect(addition(2,2)).toBe(4);
+         });  
+         it("Should return error for string values", function(){
+             expect(addition("hitchhikers", "Guide")).toBe("Error!");
+         });
+    });
+
+});
